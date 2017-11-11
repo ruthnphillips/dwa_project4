e3800612c4ccdaf2d15986875d56b0dd97dcf1e0
Ruth Phillips <ruthnphillips@gmail.com>
2017-11-11 03:26:49 -0500

    modified views for aesthetics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,12 @@
         @yield('title', 'Scouted')
     </title>
 
-	<meta charset='utf-8'>
+    <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type='text/css' rel='stylesheet'>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type='text/css' rel='stylesheet'>
     <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
-	<link href="css/scouted.css" type='text/css' rel='stylesheet'>
+    <link href="css/scouted.css" type='text/css' rel='stylesheet'>
 
     @stack('head')
 
@@ -19,13 +19,13 @@
     <div id="wrapper">
 
         <div id="header">
-        	<header>
+            <header>
                 <a href="/">
             		<img
                     src='img/logo2.png'
                     style='width:200px'
                     alt='Scouted Logo'></a>
-        	</header>
+            </header>
         </div>
 
         <div id="content">
@@ -35,7 +35,7 @@
         <div id="footer">
     	    <footer>
                 <a href='https://github.com/ruthnphillips/dwa_project4'><i class='fa fa-github'></i></a>&nbsp;
-    		    &copy; {{ date('Y') }}
+                &copy; {{ date('Y') }}
     	    </footer>
         </div>
         @stack('body')
