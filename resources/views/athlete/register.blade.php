ac733f79b771ecee5cd80db47f1cd9e711773a3e
Ruth Phillips <ruthnphillips@gmail.com>
2017-11-11 02:07:05 -0500

    change registration form display to horizontal with label

--- resources/views/athlete/register.blade.php
+++ resources/views/athlete/register.blade.php
@@ -8,71 +8,80 @@
 
 @section('content')
     <div class="container">
-        <form method='POST' action='/store-athlete'>
+        <form class="form-horizontal" method='POST' action='/store-athlete'>
 
             {{ csrf_field() }}
 
-                <!-- input to enter first name -->
-            <div class="row">
-                <div class="form-group col-sm-8">
-                    <label for="first_name">First Name </label>
-                    <input type='text' class='form-control' id='first_name' name='first_name'
-                       placeholder="(required)" value='{{old('first_name')}}' >
+            <!-- input to enter first name -->
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="first_name">First Name:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="first_name" name='first_name'
+                        placeholder="(required) First Name" required value='{{old('first_name')}}'>
                     @include('modules.error-field', ['fieldName' => 'first_name'])
                 </div>
             </div>
-                <!-- input to enter last name -->
-            <div class="row">
-                <div class="form-group col-sm-8">
-                    <label for="last_name">Last Name </label>
-                    <input type='text' class='form-control' id='last_name' name='last_name'
-                       placeholder="(required)" value='{{old('last_name')}}' >
+
+            <!-- input to enter last name -->
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="last_name">Last Name:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="last_name" name='last_name'
+                        placeholder="(required) Last Name" required value='{{old('last_name')}}'>
                     @include('modules.error-field', ['fieldName' => 'last_name'])
                 </div>
             </div>
-                <!-- input to enter email address -->
-            <div class="row">
-                <div class="form-group col-sm-8">
-                    <label for="email">email </label>
-                    <input type='text' class='form-control' id='email' name='email'
-                       placeholder="(required) name@example.com" value='{{old('email')}}' >
+
+            <!-- input to enter email address -->
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="email">email:</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" name='email'
+                        placeholder="(required) email" required value='{{old('email')}}'>
                     @include('modules.error-field', ['fieldName' => 'email'])
                 </div>
             </div>
+
             <!-- radio button to choose gender (Male/Female) -->
-            <div class='row'>
-                <div class='form-group col-sm-8'>
-                    <label>Gender</label>
-                    <div>
-                        <label class="radio-inline">
-                            <input type="radio" name="gender" value="1" @if(old('gender')) checked @endif>
-                             Male
-                        </label>
-                        <label class="radio-inline">
-                            <input type="radio" name="gender" value="0" @if(!old('gender')) checked @endif>
-                            Female
-                        </label>
-                    </div>
+            <div class='form-group'>
+                <label class="control-label col-sm-2" for="Last_name">Gender:</label>
+                <div class="col-sm-10">
+                    <label class="radio-inline">
+                        <input type="radio" name="gender" value="1" @if(old('gender')) checked @endif>
+                         Male
+                    </label>
+                    <label class="radio-inline">
+                        <input type="radio" name="gender" value="0" @if(!old('gender')) checked @endif>
+                        Female
+                    </label>
                 </div>
             </div>
-                <!-- input to enter School -->
-            <div class="row">
-                <div class="form-group col-sm-8">
-                    <label for="school_name">School Name </label>
-                    <input type='text' class='form-control' id='school_name' name='school_name'
-                        value='{{old('school_name')}}' >
+
+            <!-- input to enter School -->
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="Last_name">School:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="school_name" name='school_name'
+                        placeholder="School Name" value='{{old('school_name')}}'>
+                    @include('modules.error-field', ['fieldName' => 'school_name'])
                 </div>
             </div>
                 <!-- input to enter GPA -->
-            <div class="row">
-                <div class="form-group col-sm-8">
-                    <label for="gpa">GPA </label>
-                    <input type='text' class='form-control' id='gpa' name='gpa'
-                        placeholder="3.25" value='{{old('gpa')}}' >
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="gpa">GPA:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="gpa" name='gpa'
+                        placeholder="0-5" value='{{old('gpa')}}'>
                     @include('modules.error-field', ['fieldName' => 'gpa'])
                 </div>
             </div>
-            <input type='submit' class='btn btn-primary btn-small' value='Submit'>
+
+            <!-- submit button -->
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                  <input type='submit' class='btn btn-primary btn-small' value='Submit'>
+              </div>
+            </div>
 	    </form>
 	</div>
 @endsection
