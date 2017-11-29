00e8f08e38d43a7944e17d9d18d23b17f2b335ba
Ruth Phillips <ruthnphillips@gmail.com>
2017-11-29 10:36:38 -0500

    add plumbing for voting link

--- resources/views/layouts/main_leftbar.blade.php
+++ resources/views/layouts/main_leftbar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-sm-3 well">
     <div class="well">
+        <h3> Add your video, get discovered by scouts!</h3>
         <a href="/add-athlete" class="btn btn-primary" role="button">Register</a>
-        <p><p> Get discovered!</p></p>
     </div>
     <div class="well">
     <h4>Vote</h4>
